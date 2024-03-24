0f26eb036124c8b21400187b437b44f4c8b84fe1
mehrshad <mrfirouz007@gmail.com>
2024-03-25 00:06:09 +0330

    Fix a bug in edit distance with memory and default dictionary

--- spellchecker.py
+++ spellchecker.py
@@ -19,6 +19,10 @@ class SpellChecker:
 
         if dictionary:
             self.dictionary = dictionary
+        else:
+            dct = Dictionary()
+            dct.add_from_file('dictionaries/english.txt')
+            self.dictionary = dct
 
     def edit_distance(self, word1: str, word2: str,
                       a=-1, b=-1, memory: List[List[int]] = []) -> int:
@@ -27,14 +31,19 @@ class SpellChecker:
         if b == -1:
             b = len(word2)
 
+        if not memory:
+            memory = [[-1 for _ in range(b + 1)] for _ in range(a + 1)]
+
         if a == 0:
+            memory[a][b] = b
             return b
 
         if b == 0:
+            memory[a][b] = a
             return a
 
-        if not memory:
-            memory = [[-1 for _ in range(b + 1)] for _ in range(a + 1)]
+        if memory[a][b] != -1:
+            return memory[a][b]
 
         if word1[a - 1] == word2[b - 1]:
             if memory[a][b] != -1:
@@ -44,9 +53,6 @@ class SpellChecker:
             else:
                 memory[a][b] = self.edit_distance(word1, word2, a - 1, b - 1, memory)
                 return memory[a][b]
-
-        if memory[a][b] != -1:
-            return memory[a][b]
 
         best_case = min(self.edit_distance(word1, word2, a - 1, b, memory) if (x := memory[a - 1][b]) == -1 else x,
                         self.edit_distance(word1, word2, a, b - 1, memory) if (x := memory[a][b - 1]) == -1 else x,
--- test_spellchecker.py
+++ test_spellchecker.py
@@ -2,13 +2,19 @@ import unittest
 from spellchecker import SpellChecker
 
 
-class TestEditDistance(unittest.TestCase):
+class BaseTestCase(unittest.TestCase):
     def __init__(self, methodName: str = "runTest") -> None:
         super().__init__(methodName)
-        self.spellchecker = SpellChecker('edit_distance')
+        self.spellchecker = SpellChecker('edit_distance_dp')
 
     def score_check(self, args, value):
         self.assertEqual(self.spellchecker.get_score(*args), value)
+
+
+class TestEditDistance(BaseTestCase):
+    def __init__(self, methodName: str = "runTest") -> None:
+        super(TestEditDistance, self).__init__(methodName)
+        self.spellchecker = SpellChecker('edit_distance_recursive')
 
     def test_equal(self):
         a = 'hello'
@@ -40,6 +46,10 @@ class TestEditDistance(unittest.TestCase):
         # test change + add
         self.score_check((a, b), 2)
 
+        a = 'asset'
+        b = 'ascent'
+        self.score_check((a, b), 2)
+
     def test_long(self):
         a = 'keratinkera'
         b = 'eratinkerat'
@@ -48,17 +58,38 @@ class TestEditDistance(unittest.TestCase):
 
 class TestEditDistanceRecursive(TestEditDistance):
     def __init__(self, methodName: str = "runTest") -> None:
-        super().__init__(methodName)
+        super(TestEditDistance, self).__init__(methodName)
         self.spellchecker = SpellChecker('edit_distance_recursive')
 
 
 class TestEditDistanceDP(TestEditDistance):
     def __init__(self, methodName: str = "runTest") -> None:
-        super().__init__(methodName)
+        super(TestEditDistance, self).__init__(methodName)
         self.spellchecker = SpellChecker('edit_distance_dp')
 
+
+class TestEditDistanceSplit(BaseTestCase):
+    def test_edit_distance_1(self):
+        original_word = 'excase'
+        for word, edit_distance in self.spellchecker.slice_n_check(original_word).items():
+            self.assertEqual(edit_distance, 1)  # making sure edit distance is 1
+            self.score_check((original_word, word), 1)
+
+        original_word = 'pleaxe'
+        for word, edit_distance in self.spellchecker.slice_n_check(original_word).items():
+            self.assertEqual(edit_distance, 1)
+            self.score_check((original_word, word), 1)
+
+    def test_edit_distance_2(self):
+        original_word = 'prooress'
+        for word, edit_distance in self.spellchecker.slice_n_check(original_word).items():
+            self.score_check((original_word, word), edit_distance)
+
+        original_word = 'assent'
+        for word, edit_distance in self.spellchecker.slice_n_check(original_word).items():
+            print(word, edit_distance)
+            self.score_check((original_word, word), edit_distance)
 
 
 if __name__ == '__main__':
     unittest.main()
-
